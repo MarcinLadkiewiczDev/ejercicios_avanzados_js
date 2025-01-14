bc17d720772a9e9a618f9a17f9e8152e6ede8c5d
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-01-14 20:23:30 +0100

    completed

---
+++ ejercicio4.js
@@ -0,0 +1,43 @@
+/* 1. Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parámetro.
+
+Haz varios ejemplos y compruébalos. */
+
+const mainCharacters = [
+  "Luke",
+  "Leia",
+  "Han Solo",
+  "Chewbacca",
+  "Rey",
+  "Anakin",
+  "Obi-Wan",
+];
+
+function findArrayIndex(array, text) {
+    let index = -1;
+    if(array.includes(text)){
+        index = array.indexOf(text);
+        return index;
+    } else {
+        return "La palabra "+text+" no está en el array.";
+    }
+    
+}
+
+console.log(findArrayIndex(mainCharacters, "Manolo"));
+
+/* 2. Usando la función anterior benefíciate de poder conocer el indice del array para crear una función llamada removeItem que, 
+pasándole un array y un texto como parámetros (los mismos parámetros que en el anterior ejercicio), 
+llame a la función anteriormente creada findArrayIndex y obtén el indice para posteriormente usar la función de javascript .splice() para eliminar el elemento del array.
+
+Es decir, tienes que crear una función que elimine elementos del array y retorne el nuevo array sin el elemento, apoyándote en findArrayIndex.
+
+Finalmente retorna el array.
+
+De nuevo haz varios ejemplos para practicar y comprueba que funcionan correctamente. */
+
+function removeItem(array, text) {
+    array.splice((findArrayIndex(array, text)),1);
+    return array;
+}
+
+console.log(removeItem(mainCharacters, "Obi-Wan"));
---
+++ ejercicio5.js
@@ -0,0 +1,17 @@
+/* Crea una función llamada rollDice() que reciba como parámetro el numero de caras que queramos que tenga el dado que deberá simular el codigo dentro de la función.
+
+Que la función use el parametro para simular una tirada de dado y retornar el resultado.
+
+Si no se te ocurre como hacer un numero aleatorio no te preocupes. Busca información sobre la función de JavaScript Math.random() */
+
+
+
+function rollDice(diceFaces) {
+    const result = Math.floor((Math.random() * diceFaces) + 1);
+    return result;
+}
+
+console.log(rollDice(6));
+
+
+// En esta función uso el Math.floor para redondear y no me salga decimal. Sumo 1 ya que math floor siempre redondea a la baja, sin sumarle 1 me va a devolver siempre entre 0 y diceFaces -1;
